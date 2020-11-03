e58509f837e4bbe8186b6ee92c5e13c5ace82fac
cjzcheng@yahoo.com <cjzcheng@yahoo.com>
2020-11-03 15:25:51 -0500

    a simple component combo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import Hello from './hello';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+        <div>
+          <h1>App</h1>
+          <Hello/>
+        </div>
+      )
+  }
 }
 
 export default App;
