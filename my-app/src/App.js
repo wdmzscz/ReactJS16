801a7ced79c7cf30f0a7af2ae400c5f8d4286dd4
cjzcheng@yahoo.com <cjzcheng@yahoo.com>
2020-11-04 09:38:35 -0500

    add and list template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import Hello from './hello';
+import Add from './components/add/add';
+import List from './components/list/list';
 import './App.css';
 
 class App extends React.Component{
   render(){
     return (
         <div>
-          <h1>App</h1>
-          <Hello/>
+          <header className='site-header'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-md-12'>
+                  <h1>Please leave comments below:</h1>
+                </div>
+              </div>
+            </div>
+          </header>
+          <div className='container'>
+            <Add/>
+            <List/>
+          </div>
         </div>
       )
   }
